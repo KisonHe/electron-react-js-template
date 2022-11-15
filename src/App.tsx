0137bf2f40f8d8bf17338416c1a43b7f389daa96
KisonHe <kisonmaxine@gmail.com>
2022-11-15 10:50:39 +0800

    Init By Kison

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import isElectron from 'is-electron';
 
 function App() {
+  var Name = "Not known";
+  if (navigator.appVersion.indexOf("Win") != -1) Name =
+      "Windows OS";
+  if (navigator.appVersion.indexOf("Mac") != -1) Name =
+      "MacOS";
+  if (navigator.appVersion.indexOf("X11") != -1) Name =
+      "UNIX OS";
+  if (navigator.appVersion.indexOf("Linux") != -1) Name =
+      "Linux OS";
+  console.log(Name);
+  console.log('Is electron' + isElectron());
+
   return (
     <div className="App">
       <header className="App-header">
